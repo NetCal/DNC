7d39450a78f8f75404ae21ded7704cafbb946eb2
sbondorf <sbondorf@funk.informatik.uni-kl.de>
2017-01-10 09:53:26 +0100

    FindBugs: Ignore unread is_rate_latency and is_token_bucket in v2.3.0

--- src/unikl/disco/curves/Curve.java
+++ src/unikl/disco/curves/Curve.java
@@ -28,6 +28,8 @@
 
 package unikl.disco.curves;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -68,10 +70,12 @@ public class Curve {
 	protected boolean is_delayed_infinite_burst = false;
 
 	private boolean has_rate_latency_meta_info = false;
+	@SuppressFBWarnings(value="URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD",justification ="Will be used in a later version")
 	protected boolean is_rate_latency = false;
 	protected List<Curve> rate_latencies = new LinkedList<Curve>();
 
 	private boolean has_token_bucket_meta_info = false;
+	@SuppressFBWarnings(value="URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD",justification ="Will be used in a later version")
 	protected boolean is_token_bucket = false;
 	protected List<Curve> token_buckets = new LinkedList<Curve>();
 	/**
