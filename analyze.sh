06aa6bd4de1a1568c9e69d54c0d10d83e9d8eb1e
Philipp Schon <p_schon11@cs.uni-kl.de>
2017-06-28 16:43:27 +0200

    added files for gitlab CI

---
+++ analyze.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+uname="$(uname -a)"
+java="$(java -version 2>&1 >/dev/null | grep version)"
+lastupdate="$(sed -n '/.*upgrade.*/{x;p;d;q}; x' /var/log/apt/history.log)" #<- this may contain multiple lines
+#lastupdate="$(${lastupdate##*\n})"
+timestamp="$(date +"%H:%M:%S %d-%m-%Y")"
+sum=$(awk '{sum+=$1}END{ print sum/NR}' runtimes)
+
+#echo $sum
+#echo $uname
+#echo $java
+#echo $timestamp
+#echo $lastupdate
+
+output="$sum,$timestamp,$uname,$java,$lastupdate"
+#echo $output
+echo "$output" >> "statistics"
---
+++ build_parser.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+/**
+ * Created by philipp on 5/23/17.
+ */
+public class build_parser {
+    public static void main(String args[]) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("output_file"));
+            String line;
+            double tests = 0;
+            int success = 0;
+            while ((line = br.readLine()) != null) {
+                if (line.matches(".*Tests.*\\d+, Failures: \\d+, Errors: \\d+, Skipped: \\d+, Time.*\\d+.\\d+.*")) {
+                    System.out.println(line);
+                    String sp[] = line.split(" ");
+                    int tests2 = Integer.parseInt(sp[7].substring(0, sp[7].length() - 1));
+                    tests += tests2;
+                    //System.out.println(sp[7].substring(0, sp[7].length() - 1));
+                    int fail = Integer.parseInt(sp[9].substring(0, sp[9].length() - 1));
+                    //System.out.println(sp[9].substring(0, sp[9].length() - 1));
+                    int err = Integer.parseInt(sp[11].substring(0, sp[11].length() - 1));
+                    //System.out.println(sp[11].substring(0, sp[11].length() - 1));
+                    int skip = Integer.parseInt(sp[13].substring(0, sp[13].length() - 1));
+                    //System.out.println(sp[13].substring(0, sp[13].length() - 1));
+                    success = success + (tests2 - err - skip - fail);
+                    br.readLine();
+                }
+
+            }
+            //System.out.println(tests + " " + fail + " " + err);
+            //Output must me: "(xx.xx%) covered"
+            double result = 100 * success / tests;
+            //System.out.println(result);
+            String out = "" + result;
+            System.out.println("(" + out.substring(0, Math.min(5, out.length())) + "%) covered");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
