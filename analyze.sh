ddd4060cea1bfad8c5b8f8f163c76c26fd0e9909
Philipp Schon <p_schon11@cs.uni-kl.de>
2017-06-29 14:38:03 +0200

    modified analyze.sh

--- analyze.sh
+++ analyze.sh
@@ -3,7 +3,7 @@
 uname="$(uname -a)"
 java="$(java -version 2>&1 | grep version)"
 lastupdate="$(sed -n '/.*upgrade.*/{x;p;d;q}; x' /var/log/apt/history.log)" #<- this may contain multiple lines
-#lastupdate="$(${lastupdate##*\n})"
+lastupdate=${lastupdate##*Start-Date: }
 timestamp="$(date +"%H:%M:%S %d-%m-%Y")"
 sum=$(awk '{sum+=$1}END{ print sum/NR}' runtimes)
 
