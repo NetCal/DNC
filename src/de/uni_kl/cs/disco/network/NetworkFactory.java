cdf7d2d340403eac16f44fc02e2cbe597993c355
sbondorf <steffen@s-bondorf.de>
2017-08-20 11:37:32 +0200

    Removed public notifier from interface

--- src/de/uni_kl/cs/disco/network/NetworkFactory.java
+++ src/de/uni_kl/cs/disco/network/NetworkFactory.java
@@ -29,9 +29,9 @@
 package de.uni_kl.cs.disco.network;
 
 public interface NetworkFactory {
-	public Network createNetwork();
-
-	public Network getNetwork();
-
-	public void reinitializeCurves();
+	Network createNetwork();
+	
+	Network getNetwork();
+	
+	void reinitializeCurves();
 }
