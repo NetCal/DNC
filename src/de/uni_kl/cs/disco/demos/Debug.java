2c17d017b692f3830673ab79987691898b3db134
Philipp Schon <p_schon11@cs.uni-kl.de>
2017-08-22 16:45:02 +0200

    More Debug cases, interesting mix() Method

--- src/de/uni_kl/cs/disco/demos/Debug.java
+++ src/de/uni_kl/cs/disco/demos/Debug.java
@@ -3,9 +3,12 @@ package de.uni_kl.cs.disco.demos;
 import ch.ethz.rtc.kernel.Curve;
 import ch.ethz.rtc.kernel.Segment;
 import ch.ethz.rtc.kernel.SegmentList;
+import de.uni_kl.cs.disco.curves.ArrivalCurve;
 import de.uni_kl.cs.disco.curves.CurvePwAffine;
+import de.uni_kl.cs.disco.curves.LinearSegment;
 import de.uni_kl.cs.disco.curves.ServiceCurve;
 import de.uni_kl.cs.disco.curves.dnc.ArrivalCurve_DNC;
+import de.uni_kl.cs.disco.minplus.MinPlus;
 import de.uni_kl.cs.disco.nc.CalculatorConfig;
 import de.uni_kl.cs.disco.numbers.Num;
 
@@ -17,6 +20,7 @@ public class Debug {
         //null_curve();
         //conv_test();
         equ();
+        //mix();
     }
 
     public static void null_curve() throws Exception {
@@ -54,8 +58,8 @@ public class Debug {
 
         System.out.println(s1.toString());
         System.out.println(s2.toString());
-        //System.out.println("Output:" + OperatorUtils.apply(s1,s2, OperatorUtils.Operation.convolve, true).toString());
-        //System.out.println("Output:" + OperatorUtils.apply(s1,s2, OperatorUtils.Operation.convolve, false).toString());
+        System.out.println("Output:" + MinPlus.convolve(s1,s2, true).toString());
+        System.out.println("Output:" + MinPlus.convolve(s1,s2, false).toString());
 
         System.out.println("\nDNC\n");
         CalculatorConfig.getInstance().setCurveClass(CalculatorConfig.CurveClass.DNC);
@@ -70,16 +74,97 @@ public class Debug {
 
         System.out.println(s1.toString());
         System.out.println(s2.toString());
-        //System.out.println("Output:" + OperatorUtils.apply(s1,s2, OperatorUtils.Operation.convolve, true).toString());
-        //System.out.println("Output:" + OperatorUtils.apply(s1,s2, OperatorUtils.Operation.convolve, false).toString());
+        System.out.println("Output:" + MinPlus.convolve(s1,s2, true).toString());
+        System.out.println("Output:" + MinPlus.convolve(s1,s2, false).toString());
 
     }
 
 
     /*
-        Test for equality of different class instances when using copy constructor or copy method
+        Test for equality of two class instances when using copy constructor, copy method or others
      */
     public static void equ() {
+        ArrivalCurve a = CurvePwAffine.getFactory().createZeroArrivals();
+        ArrivalCurve b = CurvePwAffine.getFactory().createZeroArrivals();
+
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a equals b: " + (a.equals(b)));
+        System.out.println("a class equals b class: " + a.getClass().equals(b.getClass()));
+        System.out.println("a class == b class: " + (a.getClass() == b.getClass()));
+        System.out.println("hash a == hash b: " + (a.hashCode() == b.hashCode()));
+        System.out.println("");
+
+        LinearSegment s = LinearSegment.createHorizontalLine(5);
+        b.addSegment(s);
+
+        System.out.println(a.toString());
+        System.out.println(b.toString());
+
+        System.out.println("New b Segment:");
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a equals b: " + (a.equals(b)));
+        System.out.println("a class equals b class: " + a.getClass().equals(b.getClass()));
+        System.out.println("a class == b class: " + (a.getClass() == b.getClass()));
+        System.out.println("hash a == hash b: " + (a.hashCode() == b.hashCode()));
+        System.out.println("");
+
+        b = CurvePwAffine.getFactory().createArrivalCurve(a);
+
+        System.out.println("Copy Constructor:");
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a equals b: " + (a.equals(b)));
+        System.out.println("a class equals b class: " + a.getClass().equals(b.getClass()));
+        System.out.println("a class == b class: " + (a.getClass() == b.getClass()));
+        System.out.println("hash a == hash b: " + (a.hashCode() == b.hashCode()));
+        System.out.println("");
+
+        b = a.copy();
+
+        System.out.println("b = a.copy:");
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a equals b: " + (a.equals(b)));
+        System.out.println("a class equals b class: " + a.getClass().equals(b.getClass()));
+        System.out.println("a class == b class: " + (a.getClass() == b.getClass()));
+        System.out.println("hash a == hash b: " + (a.hashCode() == b.hashCode()));
+        System.out.println("");
+
+        b = new ArrivalCurve_DNC(a);
+
+        System.out.println("b = new AC(a):");
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a equals b: " + (a.equals(b)));
+        System.out.println("a class equals b class: " + a.getClass().equals(b.getClass()));
+        System.out.println("a class == b class: " + (a.getClass() == b.getClass()));
+        System.out.println("hash a == hash b: " + (a.hashCode() == b.hashCode()));
+        System.out.println("");
+
+        b = a;
+
+        System.out.println("b = a:");
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a equals b: " + (a.equals(b)));
+        System.out.println("a class equals b class: " + a.getClass().equals(b.getClass()));
+        System.out.println("a class == b class: " + (a.getClass() == b.getClass()));
+        System.out.println("hash a == hash b: " + (a.hashCode() == b.hashCode()));
+        System.out.println("");
+
+
+    }
+
+    /*
+        Test if it is possible to mix RTC and DNC Curves
+        Result: It works
+     */
+    public static void mix(){
+        CalculatorConfig.getInstance().setCurveClass(CalculatorConfig.CurveClass.DNC);
+        ServiceCurve a1 = CurvePwAffine.getFactory().createRateLatency(6,10);
+        CalculatorConfig.getInstance().setCurveClass(CalculatorConfig.CurveClass.MPA_RTC);
+        ServiceCurve a2 = CurvePwAffine.getFactory().createRateLatency(1,2);
+        try {
+            System.out.println("Output:" + MinPlus.convolve(a1,a2, true).toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
