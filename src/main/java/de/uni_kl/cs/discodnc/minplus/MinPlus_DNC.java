a66d1caf4c7a502bdc6d12acbcfa2b19f804e6dd
Zoltan Matyas <matyesz79@gmail.com>
2018-03-26 14:17:51 +0200

    Refactoring: 2nd commit

---
+++ src/main/java/de/uni_kl/cs/discodnc/minplus/MinPlus_DNC.java
@@ -0,0 +1,229 @@
+/*
+ * This file is part of the Disco Deterministic Network Calculator v2.4.0 "Chimera".
+ *
+ * Copyright (C) 2017, 2018 The DiscoDNC contributors
+ *
+ * disco | Distributed Computer Systems Lab
+ * University of Kaiserslautern, Germany
+ *
+ * http://discodnc.cs.uni-kl.de
+ *
+ *
+ * The Disco Deterministic Network Calculator (DiscoDNC) is free software;
+ * you can redistribute it and/or modify it under the terms of the 
+ * GNU Lesser General Public License as published by the Free Software Foundation; 
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+package de.uni_kl.cs.discodnc.minplus;
+
+import java.util.Set;
+
+// Due to name collisions, these classes are not imported,
+
+// they are referenced by their fully qualified names.
+//import ch.ethz.rtc.kernel.Curve;
+//import Curve;
+
+import de.uni_kl.cs.discodnc.curves.ArrivalCurve;
+import de.uni_kl.cs.discodnc.curves.CurvePwAffine;
+import de.uni_kl.cs.discodnc.curves.MaxServiceCurve;
+import de.uni_kl.cs.discodnc.curves.ServiceCurve;
+import de.uni_kl.cs.discodnc.minplus.dnc.Convolution_DNC;
+import de.uni_kl.cs.discodnc.minplus.dnc.Deconvolution_DNC;
+
+public enum MinPlus_DNC implements MinPlus {
+	
+	MIN_PLUS_DNC;
+	// --------------------------------------------------------------------------------------------------------------
+	// Min-Plus-Operation Dispatching
+	// --------------------------------------------------------------------------------------------------------------
+
+	// ------------------------------------------------------------
+	// Convolution
+	// ------------------------------------------------------------
+
+	// Service Curves
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#convolve(de.uni_kl.cs.discodnc.curves.ServiceCurve, de.uni_kl.cs.discodnc.curves.ServiceCurve)
+	 */
+	@Override
+	public ServiceCurve convolve(ServiceCurve service_curve_1, ServiceCurve service_curve_2) throws Exception {
+		return convolve(service_curve_1, service_curve_2, false);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#convolve(de.uni_kl.cs.discodnc.curves.ServiceCurve, de.uni_kl.cs.discodnc.curves.ServiceCurve, boolean)
+	 */
+	@Override
+	public ServiceCurve convolve(ServiceCurve service_curve_1, ServiceCurve service_curve_2,
+			boolean tb_rl_optimized) throws Exception {
+		// DNC and MPA_RTC curves
+		// DNC curves
+		return Convolution_DNC.convolve(service_curve_1, service_curve_2, tb_rl_optimized);
+
+	}
+
+	// Java won't let us call this method "convolve" because it does not care about
+	// the Sets' types; tells that there's already another method taking the same
+	// arguments.
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#convolve_SCs_SCs(java.util.Set, java.util.Set)
+	 */
+	@Override
+	public Set<ServiceCurve> convolve_SCs_SCs(Set<ServiceCurve> service_curves_1,
+			Set<ServiceCurve> service_curves_2) throws Exception {
+		return convolve_SCs_SCs(service_curves_1, service_curves_2, false);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#convolve_SCs_SCs(java.util.Set, java.util.Set, boolean)
+	 */
+	@Override
+	public Set<ServiceCurve> convolve_SCs_SCs(Set<ServiceCurve> service_curves_1,
+			Set<ServiceCurve> service_curves_2, boolean tb_rl_optimized) throws Exception {
+
+		if (service_curves_1.isEmpty()) {
+			return service_curves_2;
+		}
+		if (service_curves_2.isEmpty()) {
+			return service_curves_1;
+		}
+
+		// DNC curves
+		return Convolution_DNC.convolve_SCs_SCs(service_curves_1, service_curves_2, tb_rl_optimized);
+
+	}
+
+	// Arrival Curves
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#convolve(de.uni_kl.cs.discodnc.curves.ArrivalCurve, de.uni_kl.cs.discodnc.curves.ArrivalCurve)
+	 */
+	@Override
+	public ArrivalCurve convolve(ArrivalCurve arrival_curve_1, ArrivalCurve arrival_curve_2) throws Exception {
+		// DNC operations work with DNC and MPA_RTC curves
+			return Convolution_DNC.convolve(arrival_curve_1, arrival_curve_2);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#convolve(java.util.Set)
+	 */
+	@Override
+	public ArrivalCurve convolve(Set<ArrivalCurve> arrival_curves) throws Exception {
+		// DNC operations work with DNC and MPA_RTC curves
+			return Convolution_DNC.convolve(arrival_curves);
+	}
+
+	// Maximum Service Curves
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#convolve(de.uni_kl.cs.discodnc.curves.MaxServiceCurve, de.uni_kl.cs.discodnc.curves.MaxServiceCurve)
+	 */
+	@Override
+	public MaxServiceCurve convolve(MaxServiceCurve max_service_curve_1, MaxServiceCurve max_service_curve_2)
+			throws Exception {
+		// DNC operations work with DNC and MPA_RTC curves
+			return Convolution_DNC.convolve(max_service_curve_1, max_service_curve_2);
+	}
+
+	// Arrival Curves and Max Service Curves
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#convolve_ACs_MSC(java.util.Set, de.uni_kl.cs.discodnc.curves.MaxServiceCurve)
+	 */
+	@Override
+	public Set<CurvePwAffine> convolve_ACs_MSC(Set<ArrivalCurve> arrival_curves,
+			MaxServiceCurve maximum_service_curve) throws Exception {
+		// DNC operations work with DNC and MPA_RTC curves
+			return Convolution_DNC.convolve_ACs_MSC(arrival_curves, maximum_service_curve);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#convolve_ACs_EGamma(java.util.Set, de.uni_kl.cs.discodnc.curves.MaxServiceCurve)
+	 */
+	@Override
+	public Set<ArrivalCurve> convolve_ACs_EGamma(Set<ArrivalCurve> arrival_curves,
+			MaxServiceCurve extra_gamma_curve) throws Exception {
+		// DNC operations work with DNC and MPA_RTC curves
+			return Convolution_DNC.convolve_ACs_EGamma(arrival_curves, extra_gamma_curve);
+	}
+
+	// ------------------------------------------------------------
+	// Deconvolution
+	// ------------------------------------------------------------
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#deconvolve(java.util.Set, de.uni_kl.cs.discodnc.curves.ServiceCurve)
+	 */
+	@Override
+	public Set<ArrivalCurve> deconvolve(Set<ArrivalCurve> arrival_curves, ServiceCurve service_curve)
+			throws Exception {
+		return deconvolve(arrival_curves, service_curve, false);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#deconvolve(java.util.Set, de.uni_kl.cs.discodnc.curves.ServiceCurve, boolean)
+	 */
+	@Override
+	public Set<ArrivalCurve> deconvolve(Set<ArrivalCurve> arrival_curves, ServiceCurve service_curve,
+			boolean tb_rl_optimized) throws Exception {
+		// DNC operations work with DNC and MPA_RTC curves
+			return Deconvolution_DNC.deconvolve(arrival_curves, service_curve, tb_rl_optimized);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#deconvolve(java.util.Set, java.util.Set)
+	 */
+	@Override
+	public Set<ArrivalCurve> deconvolve(Set<ArrivalCurve> arrival_curves, Set<ServiceCurve> service_curves)
+			throws Exception {
+		return deconvolve(arrival_curves, service_curves, false);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#deconvolve(java.util.Set, java.util.Set, boolean)
+	 */
+	@Override
+	public Set<ArrivalCurve> deconvolve(Set<ArrivalCurve> arrival_curves, Set<ServiceCurve> service_curves,
+			boolean tb_rl_optimized) throws Exception {
+		// DNC operations work with DNC and MPA_RTC curves
+			return Deconvolution_DNC.deconvolve(arrival_curves, service_curves, tb_rl_optimized);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#deconvolve(de.uni_kl.cs.discodnc.curves.ArrivalCurve, de.uni_kl.cs.discodnc.curves.ServiceCurve)
+	 */
+	@Override
+	public ArrivalCurve deconvolve(ArrivalCurve arrival_curve, ServiceCurve service_curve) throws Exception {
+		return deconvolve(arrival_curve, service_curve, false);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#deconvolve(de.uni_kl.cs.discodnc.curves.ArrivalCurve, de.uni_kl.cs.discodnc.curves.ServiceCurve, boolean)
+	 */
+	@Override
+	public ArrivalCurve deconvolve(ArrivalCurve arrival_curve, ServiceCurve service_curve,
+			boolean tb_rl_optimized) throws Exception {
+		// DNC operations work with DNC and MPA_RTC curves
+			return Deconvolution_DNC.deconvolve(arrival_curve, service_curve);
+	}
+
+	/* (non-Javadoc)
+	 * @see de.uni_kl.cs.discodnc.minplus.IMinPlus#deconvolve_almostConcCs_SCs(java.util.Set, java.util.Set)
+	 */
+	@Override
+	public Set<ArrivalCurve> deconvolve_almostConcCs_SCs(Set<CurvePwAffine> curves,
+			Set<ServiceCurve> service_curves) throws Exception {
+		// DNC operations work with DNC and MPA_RTC curves
+			return Deconvolution_DNC.deconvolve_almostConcCs_SCs(curves, service_curves);
+	}
+	
+}
---
+++ src/main/java/de/uni_kl/cs/discodnc/nc/CurveImpl_DNC.java
@@ -0,0 +1,35 @@
+package de.uni_kl.cs.discodnc.nc;
+
+import de.uni_kl.cs.discodnc.curves.CurvePwAffine;
+import de.uni_kl.cs.discodnc.curves.LinearSegment;
+import de.uni_kl.cs.discodnc.curves.dnc.Curve_DNC;
+import de.uni_kl.cs.discodnc.curves.dnc.LinearSegment_DNC;
+import de.uni_kl.cs.discodnc.minplus.MinPlus;
+import de.uni_kl.cs.discodnc.minplus.MinPlus_DNC;
+import de.uni_kl.cs.discodnc.nc.CalculatorConfig.CurveImpl;
+import de.uni_kl.cs.discodnc.numbers.Num;
+
+public enum CurveImpl_DNC implements CurveImpl {
+		DNC;
+	
+		@Override
+		public MinPlus getMinPlus() {
+			return MinPlus_DNC.MIN_PLUS_DNC;
+		}
+
+		@Override
+		public CurvePwAffine getCurve() {
+			return Curve_DNC.getFactory();
+		}
+
+		@Override
+		public LinearSegment createLinearSegment(Num x, Num y, Num grad, boolean leftopen) {
+			return new LinearSegment_DNC(x, y, grad, leftopen);
+		}
+
+		@Override
+		public LinearSegment createHorizontalLine(double y) {
+			return new LinearSegment_DNC(Num.getFactory().createZero(),
+                    Num.getFactory().create(y), Num.getFactory().createZero(), false);
+		}
+}
