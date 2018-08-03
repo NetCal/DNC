26695d9072471275c28581c0fc1a6c0c809031c6
sbondorf <steffen@s-bondorf.de>
2018-08-03 13:48:34 -0400

    Comment formatting fixes

--- src/main/java/de/uni_kl/cs/discodnc/nc/arrivalbounds/PbooArrivalBound_Concatenation.java
+++ src/main/java/de/uni_kl/cs/discodnc/nc/arrivalbounds/PbooArrivalBound_Concatenation.java
@@ -104,11 +104,10 @@ public class PbooArrivalBound_Concatenation extends AbstractArrivalBound impleme
 		}
 
 		// Next we need to know the arrival bound of f_xfcaller at the server
-		// 'common_subpath_src', i.e., at the above sub-path's source
-		// in order to deconvolve it with beta_lo_s to get the arrival bound of the
-		// sub-path
+		// 'common_subpath_src', i.e., at the above sub-path's source in order to
+		// deconvolve it with beta_lo_s to get the arrival bound of the sub-path.
 		// Note that flows f_xfcaller that originate in 'common_subpath_src' are covered
-		// by this call of computeArrivalBound
+		// by this call of computeArrivalBound.
 		Set<ArrivalCurve> alpha_xfcaller_src = ArrivalBoundDispatch.computeArrivalBounds(network, configuration,
 				common_subpath_src, f_xfcaller, flow_of_interest);
 		Set<ArrivalCurve> alphas_xfcaller = Bound.output(configuration, alpha_xfcaller_src, common_subpath, betas_lo_subpath);
@@ -137,9 +136,9 @@ public class PbooArrivalBound_Concatenation extends AbstractArrivalBound impleme
 					continue;
 				}
 				if (alpha_xfcaller.getBurst().gt(tfa_backlog_bound_min)) {
-					alpha_xfcaller.getSegment(1).setY(tfa_backlog_bound_min); // if the burst is >0 then there are at
-					// least two segments and the second
-					// holds the burst as its y-axis value
+					// if the burst is >0 then there are at least two segments 
+					// and the second one holds the burst as its y-axis value.
+					alpha_xfcaller.getSegment(1).setY(tfa_backlog_bound_min);
 				}
 			}
 		}
