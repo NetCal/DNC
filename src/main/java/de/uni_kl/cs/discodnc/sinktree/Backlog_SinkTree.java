232d24bd1379be260358561af9f1d75ba7f25267
sbondorf <steffen@s-bondorf.de>
2018-12-11 12:12:54 +0100

    Made Util class final

--- src/main/java/de/uni_kl/cs/discodnc/sinktree/Backlog_SinkTree.java
+++ src/main/java/de/uni_kl/cs/discodnc/sinktree/Backlog_SinkTree.java
@@ -9,7 +9,7 @@ import de.uni_kl.cs.discodnc.network.server_graph.ServerGraph;
 import de.uni_kl.cs.discodnc.network.server_graph.Turn;
 import de.uni_kl.cs.discodnc.sinktree.arrivalbounds.SinkTree_AffineCurves;
 
-public abstract class Backlog_SinkTree {
+public final class Backlog_SinkTree {
 	public static double derivePmooSinkTreeAffine(ServerGraph tree, Server server,
             AnalysisConfig.ArrivalBoundMethod sink_tree_ab) throws Exception {
 		SinkTree_AffineCurves sink_tree_bound = new SinkTree_AffineCurves(tree);
