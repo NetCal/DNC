fb6bec09312a488d0a81eb81e89cd35995436d88
Steffen Bondorf <steffen@s-bondorf.de>
2019-04-02 14:38:23 +0200

    Make skipping the server backlog cap verbose
    This is theory presented in the paper MMB 2016 "Improving Cross-Traffic
    Bounds in Feed-Forward Networks – There is a Job for Everyone". But like
    the paper's evaluation, the DNC implementation is for token bucket
    arrivals only. Even if the according AnalysisConfig flag is set, it is
    only executed with the AlgDncBackend_DNC_Affine.DISCO_AFFINE backend.
    Otherwise, it was silently skipped. This skipping now triggers a
    terminal output.

--- src/main/java/org/networkcalculus/dnc/feedforward/arrivalbounds/AggregateTandemMatching.java
+++ src/main/java/org/networkcalculus/dnc/feedforward/arrivalbounds/AggregateTandemMatching.java
@@ -144,29 +144,31 @@ public class AggregateTandemMatching extends AbstractArrivalBound implements Arr
 		// TODO It has not been investigated if the TFA node backlog can improve TM arrival bounds.
 		// TODO This implementation only works for token-bucket arrivals. 
 		// It disregards the potential shift in inflection points not present in this burst cap variant.
-		if (configuration.serverBacklogArrivalBound()
-				&& Calculator.getInstance().getDncBackend() == AlgDncBackend_DNC_Affine.DISCO_AFFINE) {
-			Server last_hop_xtx = turn.getSource();
-			TotalFlowAnalysis tfa = new TotalFlowAnalysis(server_graph, configuration);
-			tfa.deriveBoundsAtServer(last_hop_xtx);
-
-			Set<Num> tfa_backlog_bounds = tfa.getServerBacklogBoundMap().get(last_hop_xtx);
-			Num tfa_backlog_bound_min = Num.getFactory(Calculator.getInstance().getNumBackend()).getPositiveInfinity();
-
-			for (Num tfa_backlog_bound : tfa_backlog_bounds) {
-				if (tfa_backlog_bound.leq(tfa_backlog_bound_min)) {
-					tfa_backlog_bound_min = tfa_backlog_bound;
+		if (configuration.serverBacklogArrivalBound()) {	
+			if(Calculator.getInstance().getDncBackend() == AlgDncBackend_DNC_Affine.DISCO_AFFINE) {
+				Server last_hop_xtx = turn.getSource();
+				TotalFlowAnalysis tfa = new TotalFlowAnalysis(server_graph, configuration);
+				tfa.deriveBoundsAtServer(last_hop_xtx);
+	
+				Set<Num> tfa_backlog_bounds = tfa.getServerBacklogBoundMap().get(last_hop_xtx);
+				Num tfa_backlog_bound_min = Num.getFactory(Calculator.getInstance().getNumBackend()).getPositiveInfinity();
+	
+				for (Num tfa_backlog_bound : tfa_backlog_bounds) {
+					if (tfa_backlog_bound.leq(tfa_backlog_bound_min)) {
+						tfa_backlog_bound_min = tfa_backlog_bound;
+					}
 				}
-			}
-
-			// Reduce the burst: Here's the limitation.
-			// It disregards the potential shift in inflection points not present in this burst cap variant.
-			for (ArrivalCurve alpha_xfcaller : alphas_xfcaller) {
-				if (alpha_xfcaller.getBurst().gt(tfa_backlog_bound_min)) {
-					alpha_xfcaller.getSegment(1).setY(tfa_backlog_bound_min); // if the burst is >0 then there are at
-					// least two segments and the second
-					// holds the burst as its y-axis value
+	
+				// Reduce the burst: Here's the limitation.
+				// It disregards the potential shift in inflection points not present in this burst cap variant.
+				for (ArrivalCurve alpha_xfcaller : alphas_xfcaller) {
+					if (alpha_xfcaller.getBurst().gt(tfa_backlog_bound_min)) {
+						// if the burst is >0 then there are at least two segments and the second holds the burst as its y-axis value.
+						alpha_xfcaller.getSegment(1).setY(tfa_backlog_bound_min); 
+					}
 				}
+			} else {
+				System.out.println("INFO: Capping output burstiness was not executed as it currently only works for DISCO_AFFINE curves.");
 			}
 		}
 		
