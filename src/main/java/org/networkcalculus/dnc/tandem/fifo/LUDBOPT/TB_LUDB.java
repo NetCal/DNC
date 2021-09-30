8d418a692408effbef67733adddf92e234954992
Alexander Scheffler <alexander.scheffler@rub.de>
2021-09-30 17:17:24 +0200

    LUDPBOPT package: The symbolic part of the LUDB computation

---
+++ src/main/java/org/networkcalculus/dnc/tandem/fifo/LUDBOPT/TB_LUDB.java
@@ -0,0 +1,19 @@
+package org.networkcalculus.dnc.tandem.fifo.LUDBOPT;
+
+public class TB_LUDB {
+    private Expression_LUDB b;
+    private Expression_LUDB r;
+    public TB_LUDB(Expression_LUDB jump, Expression_LUDB rate)
+    {
+        b = jump;
+        r = rate;
+    }
+
+    public Expression_LUDB getJump(){
+        return b;
+    }
+
+    public Expression_LUDB getRate(){
+        return r;
+    }
+}
