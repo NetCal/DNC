521ff9052146e66e5fb858e7b65a3d6c4a4d16b9
Alexander Scheffler <alexander.scheffler@rub.de>
2021-10-01 17:52:47 +0200

    Rename FIFOAnalysisResults to FIFOTandemAnalysisResults

--- src/main/java/org/networkcalculus/dnc/tandem/analyses/FIFOTandemAnalysisResults.java
+++ src/main/java/org/networkcalculus/dnc/tandem/analyses/FIFOTandemAnalysisResults.java
@@ -1,27 +1,15 @@
 package org.networkcalculus.dnc.tandem.analyses;
 
-
-import org.networkcalculus.dnc.curves.ArrivalCurve;
 import org.networkcalculus.dnc.curves.ServiceCurve;
-import org.networkcalculus.dnc.network.server_graph.Server;
 import org.networkcalculus.dnc.tandem.TandemAnalysisResults;
 import org.networkcalculus.num.Num;
 
-import java.util.Map;
 import java.util.Set;
 
-public class FIFOAnalysisResults extends TandemAnalysisResults {
+public class FIFOTandemAnalysisResults extends TandemAnalysisResults {
     protected Set<ServiceCurve> betas_e2e;
 
-    protected FIFOAnalysisResults() {
-    }
-
-    protected FIFOAnalysisResults(Num delay_bound, Num backlog_bound, Set<ServiceCurve> betas_e2e,
-                                  Map<Server, Set<ArrivalCurve>> map__server__alphas) {
-
-        super(delay_bound, backlog_bound, map__server__alphas);
-
-        this.betas_e2e = betas_e2e;
+    protected FIFOTandemAnalysisResults() {
     }
 
     @Override
